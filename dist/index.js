2936356463d8930504c6c0000828ebc75fa24e09
Belikov <l.belikov@pushapp.ru>
2023-04-30 16:05:01 +0300

    Setup body-parser

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var dotenv_1 = __importDefault(require("dotenv"));
+var body_parser_1 = __importDefault(require("body-parser"));
 var cors_1 = __importDefault(require("cors"));
 var router_1 = require("./router");
 dotenv_1.default.config();
 var app = (0, express_1.default)();
 var port = process.env.SERVER_PORT;
+app.use(body_parser_1.default.urlencoded());
+app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
 app.use('/api', router_1.router);
 app.listen(port, function () {
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from 'express'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
 import cors from 'cors'
 import { router } from './router'
 
@@ -8,6 +9,8 @@ dotenv.config()
 const app: Express = express()
 const port = process.env.SERVER_PORT
 
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
 app.use(cors())
 
 app.use('/api', router)
